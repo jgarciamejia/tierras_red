e0dc11355c2b7149e85a9545a33e6580742152b4
jgarciamejia <juliana.garcia-mejia@cfa.harvard.edu>
2022-04-15 15:35:44 -0400

    removed unnecessary dependencies from nan_test.py for ease of compilation

--- nan_test.py
+++ nan_test.py
@@ -1,18 +1,8 @@
 #!/usr/bin/env python
-from __future__ import print_function
-
-import argparse
-import math
 import numpy as np
 import sys
-
-import lfa
-import sep
-from imred import *
-import pdb
-from fitsutil import *
+import re
 import flat_field
-
 import astropy.io.fits as pyfits
 import matplotlib.pyplot as plt
 
@@ -24,12 +14,6 @@ filename = re.split('/',ffilename)[5]
 pobj = pyfits.open(ffilename)
 pobj.info()
 
-# Read trim, overscan, and smear section info from header (only one half of chip)
-hdr = pobj[1].header
-biassec = fits_section(hdr["BIASSEC"])
-smearsec = fits_section(hdr["SMEARSEC"])
-trimsec = fits_section(hdr["TRIMSEC"])
-
 # Open image data and assign NaN to overscan and smear sections via Ryan's _edges_to_nan; i.e., only illuminated pixels should remains as numbers
 img = pobj[1].data
 img_nanedges = flat_field._edges_to_nan(pobj,img,fill_value=np.nan) 
@@ -40,10 +24,12 @@ plt.imshow(img_nanedges,cmap='gray',interpolation='none')
 plt.colorbar()
 plt.show()
 
-# Trim image via trimsec
+# Trim image to only include illuminated pixels. These values come straight from trimsec as defined in the image header.  
 img_trim = img[1:1024,51:4146] #shape (1023,4095)
 
-# Plot image. Nans persist interior to the trimmed image. 
-plt.imshow(img_nanedges,cmap='gray',interpolation='none')
+# Plot image. Nans persist interior to the trimmed image, towards the edges. THAT IS THE PROBLEM! 
+plt.imshow(img_trim,cmap='gray',interpolation='none')
 plt.colorbar()
 plt.show()
+
+#hdr = pobj[1].header
