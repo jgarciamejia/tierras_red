299e07cf2cb1c2172d887293a1df678a3441cef7
jgarciamejia <juliana.garcia-mejia@cfa.harvard.edu>
2022-05-12 10:36:11 -0400

    wrote functions and commented top of code

--- bin_lc.py
+++ bin_lc.py
@@ -1,13 +1,40 @@
 import numpy as np 
 import pdb
+import pandas as pd
 
 """
-Function to bin light curve 
+Functions to bin light curve. 
+Take:
+jds - Julian Date
+rel_flux - Relative target flux. Can also be raw counts. 
+nbins - desired numebr of bins
+binsize - desired bin size, in minutes
+
+Return: 
+xbin, ybin - binned jds with bin at center of jd range 
+covered in each bin, binned fluxes weighted by sum of rel_fluxes 
+inside the bin divided by number of points in the bin. 
 """
 
-# Bin light curve by number of bins 
+def bin_lc_nbins(jds,rel_flux, nbins):
+	bins = jds[0] + binsize * np.arange(nbins) / (24*60) 
+	ybin_num = np.histogram(jds, bins=bins, weights = rel_flux)[0]
+	ybin_denom = np.histogram(jds, bins=bins)[0]
+	ybin_denom[ybin_num == 0] = 1.0
+	ybin = ybin_num / ybin_denom
+	xbin = 0.5*(bins[1:]+bins[:-1])
+	return xbin, ybin
 
-# Bin light curve by time 
+def bin_lc_binsize(jds,rel_flux, binsize):
+	nbins = (jds[-1] - jds[0])*24*60 / binsize 
+	bins = jds[0] + binsize * np.arange(nbins) / (24*60) 
+	ybin_num = np.histogram(jds, bins=bins, weights = rel_flux)[0]
+	ybin_denom = np.histogram(jds, bins=bins)[0]
+	ybin_denom[ybin_num == 0] = 1.0
+	ybin = ybin_num / ybin_denom
+	xbin = 0.5*(bins[1:]+bins[:-1])
+	return xbin, ybin
+
 date = '20220509'
 path = '/Users/jgarciamejia/Documents/TierrasProject/SCIENCE/AIJ_Output_Ryan/TOI2013_'+date+'/'
 print (path)
@@ -17,18 +44,11 @@ except FileNotFoundError:
 	df = pd.read_table(path+'toi2013_'+date+'-Tierras_1m3-I_measurements.xls')
 #pdb.set_trace()
 jds = df['J.D.-2400000'].to_numpy() 
-jds -= (2457000-2400000) 
+jds -= (2459709-2400000) 
 rel_flux = df['rel_flux_T1'].to_numpy()
 
-pdb.set_trace()
+# Bin light curve by number of bins 
+# Bin light curve by time 
+# plot and compare results
 
-binsize = 10.0 #mins
-nbin = (jds[-1] - jds[0])*24*60 / binsize 
-bins = jds[0] + binsize * np.arange(nbin+1) / (24*60) #*
-wt = 1.0 / (np.square(rel_flux_err))
-ybn = np.histogram(jds, bins=bins, weights = rel_flux*wt)[0]
-ybd = np.histogram(jds, bins=bins, weights = wt)[0]
-wb = ybd > 0 
-binned_flux = ybn[wb] / ybd[wb]
-x = 0.5*(bins[0:-1] + bins[1:])
-x = x[wb]
+pdb.set_trace()
