6217cb178ee7f2f789c02fb117f2ce6f5a307000
jgarciamejia <juliana.garcia-mejia@cfa.harvard.edu>
2022-06-08 18:59:30 -0400

    added function to calculate median and sigma when nans present in array

--- medsig.py
+++ medsig.py
@@ -5,3 +5,9 @@ def medsig(a):
   sigma = 1.482602218505601*numpy.median(numpy.absolute(a-median))
   return(median, sigma)
 
+def nanmedsig(a):
+  median = numpy.nanmedian(a)
+  sigma = 1.482602218505601*numpy.nanmedian(numpy.absolute(a-median))
+  return(median, sigma)
+
+
