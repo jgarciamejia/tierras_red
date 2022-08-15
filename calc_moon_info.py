c00c6054197a826f5902df2bf0e093e5608996dc
jgarciamejia <juliana.garcia-mejia@cfa.harvard.edu>
2022-08-15 09:57:41 -0400

    palpy calc debugging

--- calc_moon_info.py
+++ calc_moon_info.py
@@ -88,114 +88,69 @@ def main():
 
     # MOON CALCS USING PALPY 
 
+    # Calculate TT from UTC.
+    tt = utc + palpy.dtt(utc) / lfa.DAY
 
-  lat = float(config["site"]["latitude"]) * lfa.DEG_TO_RAD
-  lon = float(config["site"]["longitude"]) * lfa.DEG_TO_RAD
-  height = float(config["site"]["height"])
+    # Local Sidereal Time.
+    lst = palutil.get_lst(utc, tt, lon)
 
-  mjd = (time.time() / lfa.DAY) + lfa.JUNIX
+    # Get Sun and Moon apparent place.
+    sunrap, sundap, sundiam = palpy.rdplan(tt, 0, lon, lat)
+    moonrap, moondap, moondiam = palpy.rdplan(tt, 3, lon, lat)
 
-  logging.basicConfig(level=logging.DEBUG)
-  mjds = twilight.twilight_init(mjd, lat, lon, height)
-  
-  if mjd > mjds["midnight"]:
-    morning = True
-    ksgn = 1.0
-    flattype = "morning"
-    twillength = (mjds["offend"] - mjds["astend"]) * 86400
-    startat = mjds["nautend"]
-    calcfor = mjds["civend"]
-    mjdtimeout = mjds["offend"]
-  else:
-    morning = False
-    ksgn = -1.0
-    flattype = "evening"
-    twillength = (mjds["aststart"] - mjds["offstart"]) * 86400
-    startat = mjds["offstart"]
-    calcfor = mjds["civstart"]
-    mjdtimeout = mjds["nautstart"]
+    # Convert to geocentric apparent azimuth and elevation.
+    sunaz, sunel = palpy.de2h(lst - sunrap, sundap, lat)
+    palpy_moonaz, palpy_moonel = palpy.de2h(lst - moonrap, moondap, lat)
 
-  logging.info("Doing {0:s} flats".format(flattype))
+    # Desired pointing position, antisolar azimuth and ZD = FLAT_ZD.
+    palpy_targaz = palpy.dranrm(sunaz + math.pi)
+    palpy_targel = 0.5*math.pi - FLAT_ZD
     
-  if startat > mjd:
-    sleepfor = int(round((startat-mjd) * lfa.DAY))
-    logging.info("Wait {0:d}s for flat start".format(sleepfor))
-    time.sleep(sleepfor)
+    vt = palpy.dcs2c(az, el)
+    vm = palpy.dcs2c(moonaz, moonel)
 
-  utc = calcfor
-  
-  # Calculate TT from UTC.
-  tt = utc + palpy.dtt(utc) / lfa.DAY
+    palpy_moonsep = palpy.dsepv(vt, vm)
 
-  # Local Sidereal Time.
-  lst = palutil.get_lst(utc, tt, lon)
+    # # Check proximity to the moon.
+    # MOON_SEP_MIN = 45.0 * lfa.DEG_TO_RAD
+    # if moonsep < MOON_SEP_MIN:
+    #   # Angle by which we need to rotate.
+    #   theta = MOON_SEP_MIN - moonsep
+      
+    #   st = numpy.sin(theta)
+    #   ct = numpy.cos(theta)
 
-  # Get Sun and Moon apparent place.
-  sunrap, sundap, sundiam = palpy.rdplan(tt, 0, lon, lat)
-  moonrap, moondap, moondiam = palpy.rdplan(tt, 3, lon, lat)
+    #   # Rotate using Rodrigues' rotation formula about unit vector
+    #   # k = (vm x vt) / |vm x vt| to rotate directly away from the moon.
+    #   # vrot = ct * vt + st * k x vt + (1-ct) * (k.vt) k
+    #   # last term is always zero.
+    #   tmp = numpy.cross(vm, vt)
+    #   k = tmp / numpy.linalg.norm(tmp)
+      
+    #   vrot = ct * vt + st * numpy.cross(k, vt)
 
-  # Convert to geocentric apparent azimuth and elevation.
-  sunaz, sunel = palpy.de2h(lst - sunrap, sundap, lat)
-  moonaz, moonel = palpy.de2h(lst - moonrap, moondap, lat)
+    #   # Recompute angles.
+    #   az, el = palpy.dcc2s(vrot)
 
-  logging.info("Sun az={0:.1f} el={1:.1f}".format(sunaz * lfa.RAD_TO_DEG, sunel * lfa.RAD_TO_DEG))
-  
-  logging.info("Moon az={0:.1f} el={1:.1f}".format(moonaz * lfa.RAD_TO_DEG, moonel * lfa.RAD_TO_DEG))
-  
-  # Desired pointing position, antisolar azimuth and ZD = FLAT_ZD.
-  az = palpy.dranrm(sunaz + math.pi)
-  el = 0.5*math.pi - FLAT_ZD
+    #   # Check elevation limit.
+    #   if el < MIN_EL:
+    #     logging.info("Elevation {0:.1f} deg beyond limit, clamping".format(el * lfa.RAD_TO_DEG))
+    #     el = MIN_EL
+    #     vt = palpy.dcs2c(az, el)
+    #   else:
+    #     vt = vrot
 
-  logging.info("Antisolar position az={0:.1f} el={1:.1f}".format(az * lfa.RAD_TO_DEG, el * lfa.RAD_TO_DEG))
-  
-  vt = palpy.dcs2c(az, el)
-  vm = palpy.dcs2c(moonaz, moonel)
-
-  moonsep = palpy.dsepv(vt, vm)
-
-  logging.info("Separation from moon {0:.1f} deg".format(moonsep * lfa.RAD_TO_DEG))
-
-  # Check proximity to the moon.
-  if moonsep < MOON_SEP_MIN:
-    # Angle by which we need to rotate.
-    theta = MOON_SEP_MIN - moonsep
-
-    logging.info("Rotating by {0:.1f} deg to avoid moon".format(theta * lfa.RAD_TO_DEG))
+    #   moonsep = palpy.dsepv(vt, vm)
+      
+    #   logging.info("Final separation from moon {0:.1f} deg".format(moonsep * lfa.RAD_TO_DEG))
     
-    st = numpy.sin(theta)
-    ct = numpy.cos(theta)
+    # Transform to astrometric.
+    apha, apde = palpy.dh2e(az, el, lat)
+    apra = palpy.dranrm(lst - apha)
 
-    # Rotate using Rodrigues' rotation formula about unit vector
-    # k = (vm x vt) / |vm x vt| to rotate directly away from the moon.
-    # vrot = ct * vt + st * k x vt + (1-ct) * (k.vt) k
-    # last term is always zero.
-    tmp = numpy.cross(vm, vt)
-    k = tmp / numpy.linalg.norm(tmp)
+    centra, centde = palpy.amp(apra, apde, tt, 2000.0)
     
-    vrot = ct * vt + st * numpy.cross(k, vt)
-
-    # Recompute angles.
-    az, el = palpy.dcc2s(vrot)
-
-    # Check elevation limit.
-    if el < MIN_EL:
-      logging.info("Elevation {0:.1f} deg beyond limit, clamping".format(el * lfa.RAD_TO_DEG))
-      el = MIN_EL
-      vt = palpy.dcs2c(az, el)
-    else:
-      vt = vrot
-
-    moonsep = palpy.dsepv(vt, vm)
-    
-    logging.info("Final separation from moon {0:.1f} deg".format(moonsep * lfa.RAD_TO_DEG))
-  
-  # Transform to astrometric.
-  apha, apde = palpy.dh2e(az, el, lat)
-  apra = palpy.dranrm(lst - apha)
-
-  centra, centde = palpy.amp(apra, apde, tt, 2000.0)
-  
-  logging.info("Final position for flats {0:s} {1:s} az={2:.1f} el={3:.1f}".format(lfa.base10_to_60(centra, lfa.UNIT_RAD, ":", "", 2, lfa.UNIT_HR), lfa.base10_to_60(centde, lfa.UNIT_RAD, ":", "+", 1, lfa.UNIT_DEG), az * lfa.RAD_TO_DEG, el * lfa.RAD_TO_DEG))
+    print (palpy_targaz, palpy_targel, palpy_moonaz, lfa_moonillum, palpy_moonsep)
 
 
 if __name__ == "__main__":
