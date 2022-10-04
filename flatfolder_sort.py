465aaf5a0c905b912dca50dd92381152cd0b7454
jgarciamejia <juliana.garcia-mejia@cfa.harvard.edu>
2022-10-04 14:14:54 -0400

    modified code to sort folders assuming no previous data red attempts made

--- flatfolder_sort.py
+++ flatfolder_sort.py
@@ -8,26 +8,34 @@ import numpy
 import sys
 import os
 
+
+#def folder_sorter(folder_stype = 'flattened',date )
+
 date = sys.argv[1]
 target = sys.argv[2]
 folder1 = sys.argv[3]
-folder2 = sys.argv[4]
+#folder2 = sys.argv[4]
 
 basepath = '/data/tierras/flattened/'
-targetpath = os.path.join(basepath,date,target)
+datepath = os.path.join(basepath,date)
+targetpath = os.path.join(datepath,target)
 folder1path = os.path.join(targetpath,folder1)
-folder2path = os.path.join(targetpath,folder2)
+#folder2path = os.path.join(targetpath,folder2)
 
+if not os.path.exists(datepath):
+    print ('Creating {} Directory'.format(datepath))
+    os.system('mkdir {}'.format(datepath))
+    
 if not os.path.exists(targetpath):
     print ('Creating {} Directory'.format(targetpath))
     os.system('mkdir {}'.format(targetpath))
-    os.system('mv {} {}'.format(os.path.join(basepath,date,"*"),targetpath))
+    #os.system('mv {} {}'.format(os.path.join(basepath,date,"*"),targetpath))
     os.system('mkdir {}'.format(folder1path))
-    os.system('mv {} {}'.format(os.path.join(targetpath,"*"),folder1path))
-    os.system('mkdir {}'.format(folder2path))
+    #os.system('mv {} {}'.format(os.path.join(targetpath,"*"),folder1path))
+    #os.system('mkdir {}'.format(folder2path))
 
-else:
-    if not os.path.exists(folder1path):   
-        os.system('mkdir {}'.format(folder1path))
-        os.system('mv {} {}'.format(os.path.join(targetpath,"*"),folder1path))
-        os.system('mkdir {}'.format(folder2path))
+#else:
+#    if not os.path.exists(folder1path):   
+#        os.system('mkdir {}'.format(folder1path))
+#        os.system('mv {} {}'.format(os.path.join(targetpath,"*"),folder1path))
+#        os.system('mkdir {}'.format(folder2path))
