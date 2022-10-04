0b44d6e4832bacce1f7f33661683c0e9f76849bb
jgarciamejia <juliana.garcia-mejia@cfa.harvard.edu>
2022-10-04 14:15:46 -0400

    mod. to correctly sort never-reduced dates

--- lcfolder_sort.py
+++ lcfolder_sort.py
@@ -11,23 +11,47 @@ import os
 date = sys.argv[1]
 target = sys.argv[2]
 folder1 = sys.argv[3]
-folder2 = sys.argv[4]
+#folder2 = sys.argv[4]
 
 basepath = '/data/tierras/lightcurves/'
-targetpath = os.path.join(basepath,date,target)
-folder1path = os.path.join(targetpath,folder1)
-folder2path = os.path.join(targetpath,folder2)
 
+datepath = os.path.join(basepath,date)
+targetpath = os.path.join(datepath,target)
+folder1path = os.path.join(targetpath,folder1)
+#folder2path = os.path.join(targetpath,folder2)
+
+if not os.path.exists(datepath):
+    print ('Creating {} Directory'.format(datepath))
+    os.system('mkdir {}'.format(datepath))
+    
 if not os.path.exists(targetpath):
     print ('Creating {} Directory'.format(targetpath))
     os.system('mkdir {}'.format(targetpath))
-    os.system('mv {} {}'.format(os.path.join(basepath,date,"*"),targetpath))
+    #os.system('mv {} {}'.format(os.path.join(basepath,date,"*"),targetpath))
     os.system('mkdir {}'.format(folder1path))
-    os.system('mv {} {}'.format(os.path.join(targetpath,"*"),folder1path))
-    os.system('mkdir {}'.format(folder2path))
+    #os.system('mv {} {}'.format(os.path.join(targetpath,"*"),folder1path))
+    #os.system('mkdir {}'.format(folder2path))
 
-else:
-    if not os.path.exists(folder1path):   
-        os.system('mkdir {}'.format(folder1path))
-        os.system('mv {} {}'.format(os.path.join(targetpath,"*"),folder1path))
-        os.system('mkdir {}'.format(folder2path))
+#else:
+#    if not os.path.exists(folder1path):   
+#        os.system('mkdir {}'.format(folder1path))
+#        os.system('mv {} {}'.format(os.path.join(targetpath,"*"),folder1path))
+#        os.system('mkdir {}'.format(folder2path))
+
+#targetpath = os.path.join(basepath,date,target)
+#folder1path = os.path.join(targetpath,folder1)
+#folder2path = os.path.join(targetpath,folder2)
+
+#if not os.path.exists(targetpath):
+#    print ('Creating {} Directory'.format(targetpath))
+#    os.system('mkdir {}'.format(targetpath))
+#    os.system('mv {} {}'.format(os.path.join(basepath,date,"*"),targetpath))
+#    os.system('mkdir {}'.format(folder1path))
+#    os.system('mv {} {}'.format(os.path.join(targetpath,"*"),folder1path))
+#    os.system('mkdir {}'.format(folder2path))
+
+#else:
+#    if not os.path.exists(folder1path):   
+#        os.system('mkdir {}'.format(folder1path))
+#        os.system('mv {} {}'.format(os.path.join(targetpath,"*"),folder1path))
+#        os.system('mkdir {}'.format(folder2path))
