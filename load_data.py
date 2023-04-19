c38f60152596be837351e1842c85d7b14935e82d
jgarciamejia <juliana.garcia-mejia@cfa.harvard.edu>
2023-04-19 16:04:29 -0400

    modified structure of load_data_oneframe to save comparison star numebrs

--- load_data.py
+++ load_data.py
@@ -34,50 +34,76 @@ def calc_rel_flux(df,*exclude_comps):
 	if len(exclude_comps) == 0:
 		print ('Error: No comps selected by user for exclusion. If this is intentional, use kw: rel_flux_T1 instead of this function')
 		return None
+	used_compstars = []
 	for star in all_stars:
-		if star not in exclude_comps and star in aij_comps:
+		if not np.any(exclude_comps == star) and np.any(aij_comps == star):
+		#if star not in exclude_comps and star in aij_comps:
 			source_min_sky_comp = df['Source-Sky_C'+str(star)].to_numpy()
 			sum_source_min_sky_Cs += source_min_sky_comp
-		elif star not in exclude_comps and star not in aij_comps:
+			used_compstars.append(star) #added March 16/2023
+		if not np.any(exclude_comps == star) and not np.any(aij_comps == star):
+		#elif star not in exclude_comps and star not in aij_comps:
 			source_min_sky_comp = df['Source-Sky_T'+str(star)].to_numpy()
 			sum_source_min_sky_Cs += source_min_sky_comp
-		elif star in exclude_comps:
+			used_compstars.append(star) #added March 16/2023
+		elif np.any(exclude_comps == star):
+		#elif star in exclude_comps:
 			#print ('Comparison star No.{} excluded from rel. flux. calculation'.format(star))
 			continue
-	return source_min_sky_T1 / sum_source_min_sky_Cs
 
-def return_dataframe_onedate(mainpath,targetname,obsdate):
-	filenames = np.sort(glob.glob(mainpath+'/**/'+targetname+'**xls',recursive=True))
+	return source_min_sky_T1 / sum_source_min_sky_Cs, used_compstars
+
+def return_dataframe_onedate(mainpath,targetname,obsdate,printfnames=False):
+	filenames = np.sort(glob.glob(mainpath+'/**/'+targetname+'**measurements.xls',recursive=True))
+	#filenames = np.sort(glob.glob(mainpath+'/**/'+targetname+'**_r.xls',recursive=True)) #to read in corrected data
 	nfiles = 0
+	dfs = []
+	names = []
 	for filename in filenames:
 		if obsdate in filename:
 			nfiles += 1
-			df = pd.read_table(filename)
-			the_filename = filename
+			try:
+				dfs.append(pd.read_table(filename, encoding="utf-8"))
+			except UnicodeDecodeError:
+				dfs.append(pd.read_excel(filename))
+			names.append(filename)
 	if nfiles == 0:
 		print ('Error: No data found for {} on {}'.format(targetname,obsdate))
 	elif nfiles > 1:
-		print ('Error: More than one file found for {} on {}'.format(targetname,obsdate))
-	else:
-		return df,the_filename
+		print ('Warning: {} files found for {} on {}'.format(nfiles,targetname,obsdate))
+		if printfnames:
+			for name in names:
+				print (name)
+		return dfs,names
+	elif nfiles == 1:
+		return dfs[0],names[0]
 
-def return_data_onedate(mainpath,targetname,obsdate,threshold=3,*exclude_comps):
-	df = return_dataframe_onedate(mainpath,targetname,obsdate)[0]
-	bjds = df['BJD_TDB_MOBS'].to_numpy()
-	widths = df['Width_T1'].to_numpy()
-	try:
-		airmasses = df['AIRMASS'].to_numpy()
-	except KeyError:
-		airmasses = df[' AIRMASS'].to_numpy()
-	if exclude_comps:
-		#print ('Comp stars {} selected by user to be excluded from rel flux calc'.format(exclude_comps))
-		rel_flux_T1 = calc_rel_flux(df,*exclude_comps)
-	else:
-		rel_flux_T1 = df['rel_flux_T1'].to_numpy()
-	
-	medflux, sigflux = medsig(rel_flux_T1)
-	flag = np.absolute(rel_flux_T1 - medflux) < threshold*sigflux
-	return (df,bjds,rel_flux_T1,airmasses,widths,flag)
+def return_data_onedate(mainpath,targetname,obsdate,threshold,*exclude_comps,flag_output=False):
+	dfs,filenames = return_dataframe_onedate(mainpath,targetname,obsdate)
+	if type(dfs) == list:
+		print (dfs,len(dfs))
+		print ('Error: Attempting to unpack multiple data frames for a single date.') 
+		return None
+	elif type(dfs) == pd.core.frame.DataFrame:
+		df = dfs
+		bjds = df['BJD_TDB_MOBS'].to_numpy()
+		widths = df['Width_T1'].to_numpy()
+		try:
+			airmasses = df['AIRMASS'].to_numpy()
+		except KeyError:
+			airmasses = df[' AIRMASS'].to_numpy()
+		if exclude_comps:
+			#print ('Comp stars {} selected by user to be excluded from rel flux calc'.format(exclude_comps))
+			rel_flux_T1, comps_used = calc_rel_flux(df,*exclude_comps)
+		else:
+			rel_flux_T1 = df['rel_flux_T1'].to_numpy()
+		
+		medflux, sigflux = medsig(rel_flux_T1)
+		flag = np.absolute(rel_flux_T1 - medflux) < threshold*sigflux
+		if not flag_output:
+			return (df,bjds,rel_flux_T1,airmasses,widths,flag,comps_used)
+		elif flag_output:
+			return (df,bjds[flag],rel_flux_T1[flag],airmasses[flag],widths[flag],flag,comps_used)
 
 
 def return_data_multidate(path,targetname,dates,threshold):
@@ -85,7 +111,7 @@ def return_data_multidate(path,targetname,dates,threshold):
 #targetname: string, name of target in all caps, no spaces
 #date: list of strings, dates of observation, MST
 #threshold: int or float, threshold*sigma to produce a
-#flag array (T/F) of length glob_data	
+#flag array (T/F) of length glob_data	get_
 	glob_dateind = np.array([])
 	glob_bjds = np.array([])
 	glob_widths = np.array([])
