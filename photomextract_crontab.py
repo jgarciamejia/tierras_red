2b9efbd9eb4afe72d7962a3c0eec01c7a1a0167f
Juliana Garcia-Mejia <juliana.garcia-mejia@cfa.harvard.edu>
2024-01-19 09:54:13 -0500

    photomextract_crontab.py: beginning of bug fixes. still in testing.

--- photomextract_crontab.py
+++ photomextract_crontab.py
@@ -1,16 +1,9 @@
+import os
 import glob
 import argparse
 import numpy as np
 import ap_phot_forcrontab
 #from ap_phot import main 
-
-def get_target_list(datepath):
-    flist = os.listdir(datepath)
-    target_list = set([flist[ind].split('.')[2] for ind in range(len(flist))])
-    if 'FLAT001' in target_list:
-        target_list = [targetname for targetname in target_list if not targetname.startswith('FLAT')]
-        #print (target_list)
-    return target_list
 
 def get_yday_date():
     # Get today's date
@@ -24,17 +17,17 @@ def get_yday_date():
 
 # Access observation info
 ffname = "flat0000" # TO DO: upgrade when flats available
-date = get_yday_date()
+date = '20240115'#get_yday_date()
 live_plot = False # TO DO: upgrade if needed
 
 #Define base path
 fpath = '/data/tierras/flattened'
 
 # Get target names observed
-targets = get_target_list(os.path.join(fpath,date))
+targets = os.listdir(os.path.join(fpath,date)) #implies that only reduced/flattened targets will be extracted
 
 for target in targets:
-	folderlist = np.sort(glob.glob(os.path.join(fpath,date,target))
+	folderlist = np.sort(glob.glob(os.path.join(fpath,date,target)))
 	arg_str = f'-target {target} -date {date} -ffname {ffname} -live_plot {live_plot}'.split()
 	ap_phot_forcrontab.main(arg_str)
 
