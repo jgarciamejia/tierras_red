df76749e0cc4bf34fc6e2807b6402556170b8fa4
Patrick Tamburo <tamburop@bu.edu>
2024-11-07 09:54:39 -0500

    fixed bug in making median flat

--- glow_flat.py
+++ glow_flat.py
@@ -7,10 +7,12 @@ import argparse
 from glob import glob 
 import lfa 
 from fitsutil import * 
-from scipy.spatial import cKDTree
+from scipy.spatial import KDTree
 import pickle 
 import os 
 from ap_phot import set_tierras_permissions
+from scipy.spatial import distance_matrix
+from scipy.ndimage import median_filter
 
 def process_extension(imp, iext):
 	hdr = imp.header
@@ -45,9 +47,11 @@ def process_extension(imp, iext):
 def main(raw_args=None):
 	ap = argparse.ArgumentParser()
 	ap.add_argument("-date", required=True, help="Date of observation in YYYYMMDD format.")
+	ap.add_argument("-neighborhood_size", required=False, default=50, type=int, help='The number of pixels surrounding a given pixel to consider when correcting for glow.')
 	args = ap.parse_args()
 	date = args.date
-
+	neighborhood_size = args.neighborhood_size
+	k = neighborhood_size+1
 	path = f'/data/tierras/incoming/{date}/'
 	flat_files = sorted(glob(path+'*FLAT*.fit'))
 	n_files = len(flat_files)
@@ -56,50 +60,46 @@ def main(raw_args=None):
 	else:
 		print(f'Found {n_files} flat files in /data/tierras/incoming/{date}/')
 
-	fplist = [ pyfits.open(filename) for filename in flat_files ]
-
 	im_shape = (2048, 4096)
+	x, y = np.meshgrid(np.arange(4096), np.arange(2048))
+	coordinates = np.vstack([x.ravel(), y.ravel()]).T
+	tree = KDTree(coordinates)
 	
-	# pre-compute the nearest pixels at each pixel location 
-	k = 51
-	nearest_indices_file = f'nearest_indices_{k-1}.p'
-	if not os.path.exists(nearest_indices_file):
-		print(f'Pre-computing nearest {k-1} pixels at each pixel location...')
-		pixel_coords = np.array([(y, x) for x in range(im_shape[0]) for y in range(im_shape[1])], dtype='int32')
-		tree = cKDTree(pixel_coords)
-		distances, indices = tree.query(pixel_coords, k=k)
-		nearest_indices = indices[:,1:]
-		print(f'Saving indices to {nearest_indices_file}')
-		pickle.dump((pixel_coords, nearest_indices.astype('int32')), open(nearest_indices_file, 'wb')) # save indices to disk for faster processing in future
-	else:
-		# if the pre-computed neighbor indices have already been saved to disk, restore them
-		print(f'Restoring pre-computed {k-1}-pixel neighborhoods for each pixel location.')
-		pixel_coords, nearest_indices = pickle.load(open(nearest_indices_file, 'rb'))
-
 	combined_images = []
-	avg_images = []
-	#for i in range(n_files):
+	# loop to read in and combine flats 
+	# need to have them in memory so we can compute neighborhood pixel indices just once for each pixel location instead of n_flats times 
 	for i, filename in enumerate(flat_files):
 		print(f'Doing {filename} ({i+1} of {n_files})')
-
 		with fits.open(filename) as ifp:
 			proc_imgs = [process_extension(ifp[j], j) for j in range(1, 3)]
-		
 		combined_img = np.concatenate(proc_imgs, axis=0)
 		combined_images.append(combined_img)
 
-		# divide each pixel in the combined image by the mean of its 50 neighbors 
-		neighbor_yx = pixel_coords[nearest_indices]  # This will be a (8388608, 50, 2) array with y, x coordinates
-		neighbor_data = combined_img[neighbor_yx[:, :, 1], neighbor_yx[:, :, 0]]  # Now (8388608, 50) array with pixel values
-		avg_neighbors = np.median(neighbor_data, axis=1).reshape(im_shape)
-		avg_img = np.divide(combined_img, avg_neighbors, out=np.zeros_like(combined_img), where=avg_neighbors != 0)			
-		avg_images.append(avg_img)
-	
+	combined_images = np.array(combined_images)
+	avg_images = np.zeros_like(combined_images)	
+
+	# construct the images corrected by the median of neighboring pixels
+	n_ops = im_shape[1]*im_shape[0]
+	for i in range(n_ops):
+		coord = coordinates[i]
+		coord_data = combined_images[:, coord[1], coord[0]]
+		_, nearest_indices = tree.query(coord, k=k)
+		neighbor_yx = coordinates[nearest_indices[1:]]
+		combined_images[:, neighbor_yx[:,1], neighbor_yx[:,0]]
+		neighbor_data = np.median(combined_images[:, neighbor_yx[:,1], neighbor_yx[:,0]], axis=1) # an n_flat array of the median of the neighborhood_size-surrounding pixels 
+		avg_images[:, coord[1], coord[0]] = coord_data / neighbor_data
+		if i % 99999 == 0 and i != 0:
+			percent_complete = (i+1)/n_ops * 100
+			print(f'{percent_complete:.2f}% complete.')
+
 	print('Median-combining the glow-filtered flats...')
 	# save the median image and stddev images to /data/tierras/flats/
-	median_image = np.median(np.dstack(avg_images), axis=2) # the median-combined image (across the flat sequence)
-	stddev_image = np.std(np.dstack(avg_images), axis=2) # the standard deviation image (across the flat sequence)
-	output_path = f'/data/tierras/flats/{date}_FLAT.fit'
+	median_image = np.median(avg_images, axis=0) # the median-combined image (across the flat sequence)
+	stddev_image = np.std(avg_images, axis=0) # the standard deviation image (across the flat sequence)
+	if neighborhood_size == 50:
+		output_path = f'/data/tierras/flats/{date}_FLAT.fit'
+	else:
+		output_path = f'/data/tierras/flats/{date}_FLAT_{neighborhood_size}.fit'
 	output_hdul = fits.HDUList([fits.PrimaryHDU(data=median_image), fits.ImageHDU(data=stddev_image)])
 	output_hdul.writeto(output_path)
 	set_tierras_permissions(output_path)
